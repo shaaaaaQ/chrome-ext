c795362b2647030155cfa96190456eced5eec547
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2023-01-28 01:55:56 +0900

    a

--- src/tweetdeck/paste.js
+++ src/tweetdeck/paste.js
@@ -1,23 +1,3 @@
-/*
-
-const mr = document.createElement('script');
-mr.src = 'https://unpkg.com/moduleraid@6.0.0/dist/moduleraid.iife.js';
-document.head.appendChild(mr);
-
-const s = document.createElement('script');
-s.textContent = `
-let jquery;
-document.addEventListener('paste', (e) => {
-    if (!jquery) {
-        jquery = new ModuleRaid().findConstructor('jquery:')[0][1];
-    }
-    if (e.clipboardData.types[0] !== 'Files') return;
-    const files = [e.clipboardData.items[0].getAsFile()];
-    jquery(document).trigger('uiFilesAdded', { files });
-});
-`;
-document.head.appendChild(s);
-*/
 const mr = document.createElement('script');
 mr.src = chrome.runtime.getURL('node_modules/moduleraid/dist/moduleraid.iife.js');
 document.head.appendChild(mr);
